1d4584ea09efe5175f7c0032e67ad7d458aa9cda
metodiew <stanko@metodiew.com>
2024-01-11 19:10:19 +0200

    Initial commit of 404.php file

---
+++ wp-content/themes/softuni/404.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+    </div> <!-- /.container -->
+    <div class="nutral"></div>
+</section> <!-- /#header -->
+
+<!-- Product -->
+
+<section id="product" class="product">
+    <div class="container section-bg">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="title-box">
+                    <h2 class="title"><?php _e( '404, sorry, not found!', 'softuni' ); ?></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
