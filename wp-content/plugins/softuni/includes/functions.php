bd2d7ad3baa2a3116f6d7663633e58ba03065fc3
metodiew <stanko@metodiew.com>
2024-01-18 19:53:59 +0200

    Initial commit of our fist shortcode

--- wp-content/plugins/softuni/includes/functions.php
+++ wp-content/plugins/softuni/includes/functions.php
@@ -1,2 +1,43 @@
 <?php
 // File for generic functions
+
+$var = 123;
+
+$array = array(
+    1,
+    2,
+    3,
+    4,
+    5
+);
+
+// echo '<pre>';
+// var_dump( $array );
+// echo '</pre>';
+// die();
+
+
+/**
+ * Custom Shortcode to show the title by ID
+ *
+ * @return void
+ */
+function show_post_title_by_id( $atts ) {
+
+    $shortcode_atts = shortcode_atts(array(
+            'id' => '',
+            'option_1' => 'Option 1',
+            'option_2' => 'Option 2',
+        ),
+        $atts
+    );
+
+    $title = '';
+
+    if ( ! empty( $shortcode_atts['id'] ) ) {
+        $title = get_the_title( $shortcode_atts['id'] );
+    }
+
+    return $title;
+}
+add_shortcode( 'show_post_title', 'show_post_title_by_id' );
